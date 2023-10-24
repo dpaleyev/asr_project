e66d9b88449e970bb1a7851506066e21edec884c
Daniel Paleyev <danny.paleyev@gmail.com>
2023-10-24 23:25:52 +0300

    scheduler uploading from ckpt

--- hw_asr/base/base_trainer.py
+++ hw_asr/base/base_trainer.py
@@ -12,7 +12,7 @@ class BaseTrainer:
     Base class for all trainers
     """
 
-    def __init__(self, model: BaseModel, criterion, metrics, optimizer, config, device):
+    def __init__(self, model: BaseModel, criterion, metrics, optimizer, config, device, scheduler=None):
         self.device = device
         self.config = config
         self.logger = config.get_logger("trainer", config["trainer"]["verbosity"])
@@ -21,6 +21,7 @@ class BaseTrainer:
         self.criterion = criterion
         self.metrics = metrics
         self.optimizer = optimizer
+        self.scheduler = scheduler
 
         # for interrupt saving
         self._last_epoch = 0
@@ -142,6 +143,7 @@ class BaseTrainer:
             "epoch": epoch,
             "state_dict": self.model.state_dict(),
             "optimizer": self.optimizer.state_dict(),
+            "scheduler": self.scheduler.state_dict() if self.scheduler is not None else None,
             "monitor_best": self.mnt_best,
             "config": self.config,
         }
@@ -185,6 +187,7 @@ class BaseTrainer:
             )
         else:
             self.optimizer.load_state_dict(checkpoint["optimizer"])
+            self.scheduler.load_state_dict(checkpoint["scheduler"])
 
         self.logger.info(
             "Checkpoint loaded. Resume training from epoch {}".format(self.start_epoch)
