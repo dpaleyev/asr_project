c2a3158cf46f1db17eef5e7ade5bd621da101b4e
Daniel Paleyev <danny.paleyev@gmail.com>
2023-10-29 14:59:34 +0300

    fix

--- hw_asr/metric/__init__.py
+++ hw_asr/metric/__init__.py
@@ -1,9 +1,11 @@
-from hw_asr.metric.cer_metric import ArgmaxCERMetric, BeamSearchCERMetric
-from hw_asr.metric.wer_metric import ArgmaxWERMetric, BeamSearchWERMetric
+from hw_asr.metric.cer_metric import ArgmaxCERMetric, BeamSearchCERMetric, BeamSearchCERMetricWithLM
+from hw_asr.metric.wer_metric import ArgmaxWERMetric, BeamSearchWERMetric, BeamSearchWERMetricWithLM
 
 __all__ = [
     "ArgmaxWERMetric",
     "ArgmaxCERMetric",
     "BeamSearchWERMetric",
     "BeamSearchCERMetric",
+    "BeamSearchWERMetricWithLM",
+    "BeamSearchCERMetricWithLM",
 ]
